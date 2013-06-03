7b177ac907ae7ddd6556f0b8865bbea81e6f0436
Jonathan Spies <jonathan.spies@gmail.com>
2013-06-03 16:17:52 -0500

    added settlement report

--- lib/payflow/report.rb
+++ lib/payflow/report.rb
@@ -8,16 +8,44 @@ module Payflow
     TEST_HOST = 'payments-reports.paypal.com/test-reportingengine'
     LIVE_HOST = 'payments-reports.paypal.com/reportingengine'
 
-    def initialize
+    def initialize(options)
+      @options = options
     end
 
-    def run_report()
+    def settlement_report(processor)
       xml = Builder::XmlMarkup.new
-      xml.instruct!
-      xml.tag! 'reportName', 'DailyActivityReport'
-      xml.tag! 'reportParam' do
-        xml.tag! 'paramName', 'report_date'
-        xml.tag! 'paramValue', '2013-06-03'
+      xml.tag! 'runReportRequest' do
+        xml.tag! 'reportName', 'SettlementReport'
+
+        [
+          {name: 'processor', value: processor},
+          {name: 'start_date', value: '2013-05-03'},
+          {name: 'end_date', value: '2013-06-03'}
+        ].each do |param|
+          xml.tag! 'reportParam' do
+            xml.tag! 'paramName', param[:name]
+            xml.tag! 'paramValue', param[:value]
+          end
+        end
+      end
+
+      commit(xml.target!)
+    end
+
+    def get_meta(report_id)
+      xml = Builder::XmlMarkup.new
+      xml.tag! 'getMetaDataRequest' do
+        xml.tag! 'reportId', report_id
+      end
+
+      commit(xml.target!)
+    end
+
+    def get_report(report_id)
+      xml = Builder::XmlMarkup.new
+      xml.tag! 'getDataRequest' do
+        xml.tag! 'reportId', report_id
+        xml.tag! 'pageNum', "1"
       end
 
       commit(xml.target!)
@@ -25,8 +53,10 @@ module Payflow
 
     def commit(body)
       body = build_request(body)
+
+      puts body
+
       response = connection.post do |request|
-        add_common_headers!(request)
         request.body = body
       end
 
@@ -54,17 +84,15 @@ module Payflow
       def build_request(body)
         xml = Builder::XmlMarkup.new
         xml.instruct!
-        xml.tag! 'reportingEngineRequest', 'Timeout' => timeout.to_s, 'version' => "2.1", "xmlns" => XMLNS do
+        xml.tag! 'reportingEngineRequest' do
           xml.tag! 'authRequest' do
             xml.tag! 'user', !@options[:user].blank? ? @options[:user] : @options[:login]
-            xml.tag! 'password', @options[:password]
             xml.tag! 'vendor', @options[:login]
             xml.tag! 'partner', @options[:partner]
+            xml.tag! 'password', @options[:password]
           end
 
-          xml.tag! 'runReportRequest' do
-            xml << body
-          end
+          xml << body
         end
         xml.target!
       end
