d8a1bf83a47ab9ec49b0be973bc45ba626f8729c
Jonathan Spies <jonathan.spies@gmail.com>
2013-06-04 15:01:13 -0500

    settlement report

--- lib/payflow/report.rb
+++ lib/payflow/report.rb
@@ -1,4 +1,5 @@
 require 'builder'
+require 'nokogiri'
 
 module Payflow
   class Report
@@ -32,35 +33,19 @@ module Payflow
       commit(xml.target!)
     end
 
-    def get_meta(report_id)
-      xml = Builder::XmlMarkup.new
-      xml.tag! 'getMetaDataRequest' do
-        xml.tag! 'reportId', report_id
-      end
-
-      commit(xml.target!)
-    end
-
     def get_report(report_id)
-      xml = Builder::XmlMarkup.new
-      xml.tag! 'getDataRequest' do
-        xml.tag! 'reportId', report_id
-        xml.tag! 'pageNum', "1"
-      end
-
-      commit(xml.target!)
+      meta = parse_meta(get_meta(report_id))
+      data = parse_data(meta, get_data(report_id))
     end
 
     def commit(body)
       body = build_request(body)
 
-      puts body
-
       response = connection.post do |request|
         request.body = body
       end
 
-      puts response.inspect
+      response.body
     end
 
     def test?
@@ -95,6 +80,57 @@ module Payflow
           xml << body
         end
         xml.target!
+      end
+
+      def get_meta(report_id)
+        xml = Builder::XmlMarkup.new
+        xml.tag! 'getMetaDataRequest' do
+          xml.tag! 'reportId', report_id
+        end
+
+        commit(xml.target!)
+      end
+
+      def get_data(report_id)
+        xml = Builder::XmlMarkup.new
+        xml.tag! 'getDataRequest' do
+          xml.tag! 'reportId', report_id
+          xml.tag! 'pageNum', "1"
+        end
+
+        commit(xml.target!)
+      end
+
+      def parse_meta(xml)
+        doc = Nokogiri::XML(xml)
+
+        columns = []
+        doc.xpath("//columnMetaData").each do |node|
+          columns << node.xpath(".//dataName").text
+        end
+
+        {
+          responseCode: doc.xpath("//responseCode").text,
+          numberOfPages: doc.xpath("//numberOfPages").text,
+          perPage: doc.xpath("//pageSize").text,
+          columns: columns
+        }
+      end
+
+      def parse_data(meta, xml)
+        doc = Nokogiri::XML(xml)
+
+        report = []
+        doc.xpath("//reportDataRow").each do |row|
+          row_data = {}
+          row.xpath(".//columnData").each do |column|
+            column_number = column.attr("colNum").to_i - 1
+            row_data[meta[:columns][column_number]] = column.xpath(".//data").text
+          end
+          report << row_data
+        end
+
+        report
       end
   end
 end
