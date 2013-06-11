42b90572fe9e72cf76d7643df774c560a0575874
Justin Litchfield <litch@me.com>
2013-06-11 09:51:31 -0500

    will return a declined transaction if the amount is 100.01

--- lib/payflow/mock_response.rb
+++ lib/payflow/mock_response.rb
@@ -1,6 +1,13 @@
 module Payflow
   class MockResponse < Payflow::Response
     def initialize(request_body=nil)
+      request = parse(request_body)
+      generate_successful_response
+      generate_failed_response if request[:"amt[6]"] == "100.01"
+    end
+
+    private
+    def generate_successful_response
       @result = {
         result: "0",
         message: "Successful",
@@ -8,6 +15,32 @@ module Payflow
         avs_result: "0",
         cv_result: "0"
       }
+    end
+
+    def generate_failed_response
+      @result = {
+        result: "1",
+        message: "DECLINED",
+        pn_ref: "MOCKTOKEN",
+        avs_result: "0",
+        cv_result: "0"
+      }
+    end
+
+
+    def parse(request_body)
+      pairs = request_body.split("&")
+      response = {}
+
+      pairs.each do |node|
+        parse_element(response, node)
+      end
+
+      response
+    end
+
+    def parse_element(response, pair)
+      response[pair.split("=").first.underscore.to_sym] = pair.split("=").last
     end
   end
 end
--- spec/payflow/mock_response_spec.rb
+++ spec/payflow/mock_response_spec.rb
@@ -10,4 +10,13 @@ describe Payflow::Response do
     response = Payflow::MockResponse.new("RESULT=0")
     response.authorization_token.should eql("MOCKTOKEN")
   end
+
+  it "should not be successful if the amount is 100.01" do
+    request_string = "TRXTYPE[1]=S&AMT[6]=100.01&TENDER[1]=C&ORIGID[13]=CREDITCARDREF&VENDOR[0]=&PARTNER[0]=&PWD[0]=&USER[0]=
+.TRXTYPE[1]=S&AMT[6]=100.01&TENDER[1]=C&ORIGID[13]=CREDITCARDREF&VENDOR[0]=&PARTNER[0]=&PWD[0]=&USER[0]="
+    response = Payflow::MockResponse.new(request_string)
+    response.successful?.should be(false)
+  end
+
+
 end
